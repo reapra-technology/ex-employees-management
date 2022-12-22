7111cc459fe15fc877123b8feff8c84539641d38
Shinnosuke_13 <shin2002513@icloud.com>
2022-12-22 11:28:49 +0900

    remove basic auth code for dev

--- src/utils/basicAuthCheck.ts
+++ src/utils/basicAuthCheck.ts
@@ -1,18 +1,20 @@
-import initializeBasicAuth from 'nextjs-basic-auth'
-import type { IncomingMessage, ServerResponse } from 'http'
+import initializeBasicAuth from 'nextjs-basic-auth';
+import type { IncomingMessage, ServerResponse } from 'http';
 
 const users = [
-  { user: process.env.NEXT_PUBLIC_BASIC_AUTH_USER_NAME!, password: process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD! },
-  { user: 'admin', password: 'admin' },// デプロイ時に削除
-]
+  {
+    user: process.env.NEXT_PUBLIC_BASIC_AUTH_USER_NAME!,
+    password: process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD!,
+  },
+];
 
 const basicAuthCheck = async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
-  await initializeBasicAuth({ users })(req, res)
+  await initializeBasicAuth({ users })(req, res);
 
   // Workaround for major bug: If you cancel, the page loads normally. - https://github.com/jchiatt/nextjs-basic-auth/issues/4
   if (res.statusCode === 401) {
-    res.end('<html>Unauthorized</html>')
+    res.end('<html>Unauthorized</html>');
   }
-}
+};
 
-export default initializeBasicAuth({ users })
+export default initializeBasicAuth({ users });
