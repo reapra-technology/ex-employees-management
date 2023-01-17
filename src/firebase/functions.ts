67e1e7bae99a09b99fb3aa5c5fa8ac6e6638c9be
Shinnosuke_13 <shin2002513@icloud.com>
2023-01-17 15:52:33 +0900

    firebase errorログの追加

--- src/firebase/functions.ts
+++ src/firebase/functions.ts
@@ -5,18 +5,24 @@ import User from '@/types/user';
 
 export async function fetchSettingFromDB(): Promise<SettingParameter | undefined> {
   const settingsCollectionRef = collection(db, 'settings');
-  const result = await getDocs(settingsCollectionRef);
-  if (result.empty) {
+  const result = await getDocs(settingsCollectionRef).catch((err): undefined => {
+    console.log(err);
+    return undefined;
+  });
+  if (result === undefined || result.empty) {
     return undefined;
   }
   return result.docs[0].data() as SettingParameter;
 }
 
 export async function fetchUsersFromDB(): Promise<User[]> {
-  const userCollectionRef = query(collection(db, 'users'), orderBy('createdAt', 'asc'));
-  const result = await getDocs(userCollectionRef);
+  const userCollectionRef = query(collection(db, 'users'), orderBy('createdAt', 'desc'));
+  const result = await getDocs(userCollectionRef).catch((err): undefined => {
+    console.log(err);
+    return undefined;
+  });
   let users: User[] = [];
-  if (result.empty) {
+  if (result === undefined || result.empty) {
     return [];
   }
   result.docs.forEach((doc) => {
@@ -27,34 +33,49 @@ export async function fetchUsersFromDB(): Promise<User[]> {
 
 export async function updateSetting(newSetting: SettingParameter): Promise<void> {
   const ref = doc(db, `settings/${newSetting.setting_id}`);
-  await setDoc(ref, newSetting).then(() => {});
+  await setDoc(ref, newSetting)
+    .then(() => {})
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 export async function createUser(user: User): Promise<void> {
   const ref = doc(db, `users/${user.id}`);
-  await setDoc(ref, user);
+  await setDoc(ref, user).catch((err) => {
+    console.log(err);
+  });
 }
 
 export async function updateUserStateOnDB(user: User): Promise<void> {
   const ref = doc(db, `users/${user.id}`);
-  await setDoc(ref, user, { merge: true });
+  await setDoc(ref, user, { merge: true }).catch((err) => {
+    console.log(err);
+  });
 }
 
 export async function deleteUserOnDb(id: string): Promise<void> {
   const ref = doc(db, `users/${id}`);
-  await deleteDoc(ref);
+  await deleteDoc(ref).catch((err) => {
+    console.log(err);
+  });
 }
 
 export async function archiveUser(user: User): Promise<void> {
   const ref = doc(db, `archivedUser/${user.id}`);
-  await setDoc(ref, user);
+  await setDoc(ref, user).catch((err) => {
+    console.log(err);
+  });
 }
 
 export async function getExecutedUsersFromDB(): Promise<User[]> {
-  const collectionRef = query(collection(db, 'archivedUser'), orderBy('createdAt', 'asc'));
-  const result = await getDocs(collectionRef);
+  const collectionRef = query(collection(db, 'archivedUser'), orderBy('createdAt', 'desc'));
+  const result = await getDocs(collectionRef).catch((err): undefined => {
+    console.log(err);
+    return undefined;
+  });
   let users: User[] = [];
-  if (result.empty) {
+  if (result === undefined || result.empty) {
     return [];
   }
   result.docs.forEach((doc) => {
